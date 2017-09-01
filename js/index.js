282138bc725f83cbc39b35b176cd1cc982d25726
Punchapon Pumpuang <punchapon.pupu@g.swu.ac.th>
2017-09-01 14:08:25 +0700

    Edit js file.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var caps = false;
 var shift = false;
 
 function myFunction(key) {
-  var text = document.getElementById("display").innerHTML;
+  var text = document.getElementById("display").value;
   if (key == "Shift") {
     if (shift == true) {
       shift = false;
@@ -22,56 +22,56 @@ function myFunction(key) {
       document.getElementById("caps").style.backgroundColor = "dimgray";
     }
   }else if (key == "Space") {
-    document.getElementById("display").innerHTML += ' ';
+    document.getElementById("display").value += ' ';
   }else if (key == "Tab") {
-    document.getElementById("display").innerHTML += "\t";
+    document.getElementById("display").value += "\t";
   }else if (key == "Enter") {
-    document.getElementById("display").innerHTML += "<br />";
+    document.getElementById("display").value += "\n";
   }else if (key == "Backspace") {
-    document.getElementById("display").innerHTML = text.substr(0, text.length - 1);
+    document.getElementById("display").value = text.substr(0, text.length - 1);
   }else {
     if (shift) {
-
-      document.getElementById("display").innerHTML += key.toUpperCase();
+      document.getElementById("display").value += key.toUpperCase();
       document.getElementById("shfl").style.backgroundColor = "black";
       document.getElementById("shfr").style.backgroundColor = "black";
       shift = false;
     }else if (caps) {
-      document.getElementById("display").innerHTML += key.toUpperCase();
+      document.getElementById("display").value += key.toUpperCase();
     }else {
-
-      document.getElementById("display").innerHTML += key.toLowerCase();
+      document.getElementById("display").value += key.toLowerCase();
     }
   }
+document.getElementById('display').scrollTop = document.getElementById('display').scrollHeight;
 }
 
 function split(key) {
   if (shift) {
-    if (key == "`")  document.getElementById("display").innerHTML += "~";
-    else if (key == "1")  document.getElementById("display").innerHTML += "!";
-    else if (key == "2")  document.getElementById("display").innerHTML += "@";
-    else if (key == "3")  document.getElementById("display").innerHTML += "#";
-    else if (key == "4")  document.getElementById("display").innerHTML += "$";
-    else if (key == "5")  document.getElementById("display").innerHTML += "%";
-    else if (key == "6")  document.getElementById("display").innerHTML += "^";
-    else if (key == "7")  document.getElementById("display").innerHTML += "&";
-    else if (key == "8")  document.getElementById("display").innerHTML += "*";
-    else if (key == "9")  document.getElementById("display").innerHTML += "(";
-    else if (key == "0")  document.getElementById("display").innerHTML += ")";
-    else if (key == "-")  document.getElementById("display").innerHTML += "_";
-    else if (key == "=")  document.getElementById("display").innerHTML += "+";
-    else if (key == "[")  document.getElementById("display").innerHTML += "{";
-    else if (key == "]")  document.getElementById("display").innerHTML += "}";
-    else if (key == "\\")  document.getElementById("display").innerHTML += "|";
-    else if (key == ";")  document.getElementById("display").innerHTML += ":";
-    else if (key == "'")  document.getElementById("display").innerHTML += '"';
-    else if (key == ",")  document.getElementById("display").innerHTML += "&lt;";
-    else if (key == ".")  document.getElementById("display").innerHTML += "&gt;";
-    else if (key == "\/")  document.getElementById("display").innerHTML += "?";
+    if (key == "`")  document.getElementById("display").value += "~";
+    else if (key == "1")  document.getElementById("display").value += "!";
+    else if (key == "2")  document.getElementById("display").value += "@";
+    else if (key == "3")  document.getElementById("display").value += "#";
+    else if (key == "4")  document.getElementById("display").value += "$";
+    else if (key == "5")  document.getElementById("display").value += "%";
+    else if (key == "6")  document.getElementById("display").value += "^";
+    else if (key == "7")  document.getElementById("display").value += "&";
+    else if (key == "8")  document.getElementById("display").value += "*";
+    else if (key == "9")  document.getElementById("display").value += "(";
+    else if (key == "0")  document.getElementById("display").value += ")";
+    else if (key == "-")  document.getElementById("display").value += "_";
+    else if (key == "=")  document.getElementById("display").value += "+";
+    else if (key == "[")  document.getElementById("display").value += "{";
+    else if (key == "]")  document.getElementById("display").value += "}";
+    else if (key == "\\")  document.getElementById("display").value += "|";
+    else if (key == ";")  document.getElementById("display").value += ":";
+    else if (key == "'")  document.getElementById("display").value += '"';
+    else if (key == ",")  document.getElementById("display").value += "<";
+    else if (key == ".")  document.getElementById("display").value += ">";
+    else if (key == "\/")  document.getElementById("display").value += "?";
     document.getElementById("shfl").style.backgroundColor = "black";
     document.getElementById("shfr").style.backgroundColor = "black";
     shift = false;
   } else {
-    document.getElementById("display").innerHTML += key;
+    document.getElementById("display").value += key;
   }
+  document.getElementById('display').scrollTop = document.getElementById('display').scrollHeight;
 }
